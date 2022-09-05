4d1c4e47b14d29d926656f0c5d77d6c5dde78039
hi-june <wnsgur735@gmail.com>
2022-09-05 23:22:39 +0900

    Section 05.회원 관리 예제(웹 MVC 개발)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // Controller라는 어노테이션이 있으면, 스프링이 스프링 컨테이너(스프링 뜰 때 생김)에 객체를 생성해서 넣어두고 관리 == 스프링 컨테이너에서 스프링 빈이 관리된다
 public class MemberController {
@@ -27,4 +33,28 @@ public class MemberController {
     public MemberController(MemberService memberService) {  // 오류 뜨는 이유: 서비스에 어노테이션이 등록되어 있지 않다.
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();   // 멤버를 생성하고
+        member.setName(form.getName()); // 폼으로부터 이름을 가져와 member의 이름을 설정하고
+
+        memberService.join(member); // memberService를 통해 생성한 member를 저장한다.
+
+        return "redirect:/";    // 회원가입이 끝나고 home 화면으로 보냄.
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
